22a5b397b8b8055b1201258652b45dbc86d78414
236663 <smatthewbloodson@gmail.com>
2022-06-29 00:45:32 +0200

    Some logic

--- tools/src/main.cpp
+++ tools/src/main.cpp
@@ -1,6 +1,8 @@
-#include <iostream>
+#include <filesystem>
 #include <windows.h>
+#include <iostream>
 #include <fstream>
+#include <vector>
 
 #include "sample.hpp"
 #include "lib_sample.hpp"
@@ -13,35 +15,80 @@
 //    - libs
 //  - will setup project path
 
-using std::fstream;
-using std::string;
+// Error checking
+// output
+// If in file defined variables would be always in the same order 
+//  the parse function could work as more then a 1 function that would be 
+//  changed constanly in a loop for functions to look for other tokens. 
+//  Also yeah that can be easier implemented... 
 
-int setup() {
+using std::ifstream;
+using std::ofstream;
+using std::string;
+using std::vector;
+
+std::string parse(std::string line, const string projectPath) {
 	const string projectPathToken = "project_path";
+	bool isToken = false;
+	size_t i = 0;
 	
-	fstream secretFile(".ninja\\secret.ninja");
-	string currentLine;
-	
-	if (secretFile.is_open())
-		
-		while (getline(secretFile, currentLine)) {
-			std::cout << currentLine << '\n';
-			for (size_t i = 0; i < projectPathToken.length(); i++)
-				if (!(currentLine[i] == projectPathToken[i])) break;
-				else std::cout << "T";
-				// get eqaul sign with or witout spaces and write after that logic.
-		} 
-		
-	else {
-		std::cout << "ERROR : File not found!";
+	for (; i < projectPathToken.length(); i++) {
+		if (line[i] == projectPathToken[i]) isToken = true;
+		else { 
+			isToken = false; 
+			break; 
+		}
 	}
 	
-	secretFile.close();
+	if (isToken) {
+		string temp;
+		temp += "project_path = ";
+		temp += projectPath;
+		temp += "\\\n";
+		return temp;
+	}
+	
+	line += '\n';
+	return line ;
+}
+
+int setup(const string projectPath) {
+	const string originPath = ".ninja\\secret.ninja",
+		resultPath = ".ninja\\_secret.ninja";
+	
+	vector<string> data;
+	string line;
+
+	ifstream originFile(originPath);
+	ofstream resultFile(resultPath);
+
+	// READ
+	if (originFile.is_open()) {
+		while (getline(originFile, line))
+			data.push_back(line);
+		originFile.close();
+	} else std::cout << "File-Open-Error\n";
+
+	// WRITE
+	for (size_t i = 0; i < data.size(); i++)
+		resultFile << parse(data[i], projectPath);
+	resultFile.close();
+	
+	// REPLACE (Is this the safest way?)
+	std::filesystem::remove(originPath);
+	std::filesystem::rename(resultPath, originPath);
+	
 	return 0;
 }
 
-int main() {
-	setup();
+// Specific to windows unicode main function.
+// int wmain(int argumentsLength, wchar_t* arguments[])
+	
+int main(int argumentsLength, char* arguments[]) {
+	std::cout << "ProjectPath = "<< arguments[1] << "\\\n";
+	
+	setup(arguments[1]);
+	
 	std::cin.get();
 	return 0;
 }
