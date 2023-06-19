1a094d243bf2529406d9e5cb3accd4552e060c7b
dotBlueShoes <matthewstrumillo@gmail.com>
2023-06-19 17:37:26 +0200

    todo & help

--- tools/inc/Framework.hpp
+++ tools/inc/Framework.hpp
@@ -1,3 +1,4 @@
+#pragma once
 #include <filesystem>
 #include <windows.h>
 #include <iostream>
--- tools/src/main.cpp
+++ tools/src/main.cpp
@@ -1,6 +1,10 @@
 #include "Framework.hpp"
 #include "NppProcessor.hpp"
 
+/// TODO
+/// 1. Make it so tools can generate new projects based on the cpp_npp_clang_ninja setup.
+/// 2. Think about including mold in it.
+
 // 1. re-create tools.npp file so it will contain all the files there are in those folders.
 // 2. setup script that will
 //  - ask the user for:
@@ -8,6 +12,7 @@
 //    - build name
 //    - libs
 //  - will setup project path
+// 3. uppercase support.
 
 // Error checking
 // output
@@ -16,7 +21,10 @@
 //  changed constanly in a loop for functions to look for other tokens. 
 //  Also yeah that can be easier implemented... 
 
-string parseSecret(string line, const string projectPath) {
+string parseSecret(
+	string line, 
+	const string projectPath
+) {
 	const string projectPathToken { "projectPath" };
 	bool isToken { false };
 	size_t i = 0;
@@ -42,7 +50,17 @@ string parseSecret(string line, const string projectPath) {
 	return line ;
 }
 
-int setup(const string projectPath) {
+int DisplayHelp() {
+	std::cout << "Tools\n"
+		"-h, --help 		: help\n"
+		"-s, --setup 		: setup\n"
+		"-r, --refresh-npp	: refresh" << std::endl;
+	return 0;
+}
+
+int setup(
+	const string projectPath
+) {
 	const string originPath { ".ninja\\secret.ninja" },
 		resultPath { ".ninja\\_secret.ninja" };
 	
@@ -83,54 +101,95 @@ int setup(const string projectPath) {
 // int wmain(int argumentsLength, wchar_t* arguments[])
 
 namespace tokens {
+	
 	const string 
+		helpFull = "--help", helpShort = "-h",
 		setupFull = "--setup", setupShort = "-s",
 		reprojectFull = "--refresh-npp", reprojectShort = "-r";
+		
+	const string tokens[] {
+		helpFull, helpShort,
+		setupFull, setupShort,
+		reprojectFull, reprojectShort,
+	};
+	
 }
 	
-int main(const int argumentsLength, const char** arguments) {
-	bool isEqual = true;
+int main(
+	const int argumentsLength, 
+	const char** arguments
+) {
+	bool isToken = true;
 	
-	for (size_t i = 0; arguments[1][i] != '\0'; i++)
-		if (arguments[1][i] != tokens::setupFull[i]) {
-			isEqual = false;
-			break;
-		}
-		
-	if (isEqual) { setup(arguments[2]); return 0;}
-	isEqual = true;
-		
-		
-	for (size_t i = 0; arguments[1][i] != '\0'; i++)
-		if (arguments[1][i] != tokens::setupShort[i]) {
-			isEqual = false;
-			break;
-		}
-		
-	if (isEqual) { setup(arguments[2]); return 0; }
-	isEqual = true;
-	
-	
-	for (size_t i = 0; arguments[1][i] != '\0'; i++)
-		if (arguments[1][i] != tokens::reprojectFull[i]) {
-			isEqual = false;
-			break;
-		}
-		
-	if (isEqual) { 
-		const char* projects[] { arguments[3], arguments[4] }; 
-		NppProcessor::refreshNpp(arguments[2], argumentsLength - 3, projects); 
+	if (argumentsLength == 1) {
+		DisplayHelp();
 		return 0;
-	} isEqual = true;
-		
-		
+	}
+	
+	// COMMAND "--help"
 	for (size_t i = 0; arguments[1][i] != '\0'; i++)
-		if (arguments[1][i] != tokens::reprojectShort[i]) {
-			isEqual = false;
+		if (arguments[1][i] != tokens::tokens[0][i]) {
+			isToken = false;
 			break;
 		}
 		
-	if (isEqual) { 
+	if (isToken) { DisplayHelp(); }
+	
+	// COMMAND "-h"
+	isToken = true;
+	for (size_t i = 0; arguments[1][i] != '\0'; i++)
+		if (arguments[1][i] != tokens::tokens[1][i]) {
+			isToken = false;
+			break;
+		}
+		
+	if (isToken) { DisplayHelp(); }
+	
+	// COMMAND "--setup"
+	for (size_t i = 0; arguments[1][i] != '\0'; i++)
+		if (arguments[1][i] != tokens::tokens[2][i]) {
+			isToken = false;
+			break;
+		}
+		
+	if (isToken) { setup(arguments[2]); return 0; }
+	
+	// COMMAND "-s"
+	isToken = true;
+	for (size_t i = 0; arguments[1][i] != '\0'; i++)
+		if (arguments[1][i] != tokens::tokens[3][i]) {
+			isToken = false;
+			break;
+		}
+		
+	if (isToken) { setup(arguments[2]); return 0; }
+	
+	
+	// COMMAND "--refresh-npp"
+	isToken = true;
+	for (size_t i = 0; arguments[1][i] != '\0'; i++)
+		if (arguments[1][i] != tokens::tokens[4][i]) {
+			isToken = false;
+			break;
+		}
+		
+	if (isToken) { 
+		const char* projects[] { arguments[3], arguments[4] }; 
+		{ using namespace NppProcessor;
+			refreshNpp(arguments[2], argumentsLength - 3, projects); 
+		}
+		return 0;
+	} 
+	
+	// COMMAND "-r"
+	isToken = true;
+	for (size_t i = 0; arguments[1][i] != '\0'; i++)
+		if (arguments[1][i] != tokens::tokens[5][i]) {
+			isToken = false;
+			break;
+		}
+		
+	if (isToken) { 
 		const char* projects[] { arguments[3], arguments[4] }; 
 		{ using namespace NppProcessor;
 			refreshNpp(arguments[2], argumentsLength - 3, projects); 
@@ -138,7 +197,5 @@ int main(const int argumentsLength, const char** arguments) {
 		return 0;
 	}
 	
-	
-	std::cin.get();
 	return 0;
 }
